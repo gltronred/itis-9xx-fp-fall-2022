dafcfc9231c13d253f032335bb8436dd659a651f
Mansur Ziiatdinov <gltronred@gmail.com>
2022-12-16 15:02:48 +0300

    Попытки 0 и 1 написать ООП в стиле ФП

--- proj4/app/Main.hs
+++ proj4/app/Main.hs
@@ -61,6 +61,68 @@ breakTableLeg2 = map breakTableInRoom
     breakTable :: Table -> Table
     breakTable t = t{ legs = legs t - 1 }
 
+--------------------------------------------------------
+
+-- Попытка написать ООП в стиле ФП
+data LensTry0 s a = LensTry0
+  { getter :: s -> a
+  , setter :: a -> s -> s
+  }
+
+_fst0 :: LensTry0 (a,b) a
+_fst0 = LensTry0
+  { getter = \(a,b) -> a
+  , setter = \newA (oldA,oldB) -> (newA,oldB)
+  }
+
+ix0 :: Int -> LensTry0 [a] a
+ix0 i = LensTry0
+  { getter = (!! i)
+  , setter = setIdx i
+  }
+
+setIdx :: Int -> a -> [a] -> [a]
+setIdx idx new list
+  | idx < 0 = error "negative index"
+  | idx>= 0 && null list = error "index too large"
+  | otherwise = case list of
+      old:rest -> if idx == 0
+                  then new : rest
+                  else old : setIdx (idx-1) new rest
+
+-- Что, если мы хотим изменить список?
+-- modifyList :: (a -> a) -> [a] -> [a]
+--
+-- Либо копируем setIdx,
+-- либо дважды обходим (getter, потом setter)
+--
+-- Неудачная идея LensTry0, поменяем
+
+type LensTry1 s a = (a -> a) -> s -> (a, s)
+
+toLensTry0 :: LensTry1 s a -> LensTry0 s a
+toLensTry0 modifier = LensTry0
+  { getter = fst . modifier id
+  , setter = \new oldS -> snd $ modifier (const new) oldS }
+
+ix1 :: Int -> LensTry1 [a] a
+ix1 idx f list
+  | idx < 0 = error "negative index"
+  | idx>= 0 && null list = error "index too large"
+  | otherwise = case list of
+      old:rest
+        | idx==0 -> (old, f old : rest)
+        | otherwise -> let
+            (oldA, newRest) = ix1 (idx-1) f rest
+            in (oldA, old : newRest)
+
+_fst1 :: LensTry1 (a,b) a
+_fst1 f (a,b) = (a, (f a, b))
+
+-- Плохо композируются: из (s -> (a,s)) не очень похоже
+-- на (a -> a)
+
+
 main :: IO ()
 main = do
   putStrLn "Hello, Haskell!"
