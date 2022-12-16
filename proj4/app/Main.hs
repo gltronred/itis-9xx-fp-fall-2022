ede7cdb76feca95897503b4525a36b4168e5f8d8
Mansur Ziiatdinov <gltronred@gmail.com>
2022-12-16 14:16:18 +0300

    Example

--- proj4/app/Main.hs
+++ proj4/app/Main.hs
@@ -22,6 +22,25 @@ data Person = Person
   }
   deriving (Eq,Show,Read)
 
+rooms :: [Room]
+rooms = [ Room{ table=Just $ Table{ legs=3, colour=White }
+              , persons=
+                [ Person{ surname="A"
+                        , name="B"
+                        , patronym=Just "C"}
+                , Person{ surname="D"
+                        , name="E"
+                        , patronym=Nothing}]}
+        , Room{ table=Nothing
+              , persons=
+                [ Person{ surname="G"
+                        , name="H"
+                        , patronym=Just "I"} ]}]
+
+-- сломать ножку у каждого стола в каждой комнате
+breakTableLeg :: [Room] -> [Room]
+breakTableLeg _ = error "Implement!"
+
 main :: IO ()
 main = do
   putStrLn "Hello, Haskell!"
